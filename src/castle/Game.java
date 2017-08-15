d6c8b3b1328517b7a1a9b871ac3fa68217d8561c
AzureDreamJ <caohejingguipinglu@126.com>
2017-08-15 20:49:49 +0800

    third commit

--- src/castle/Game.java
+++ src/castle/Game.java
@@ -1,8 +1,8 @@
 /*
  * 网易云课堂-->Java应用基础：设计篇-->第4章、设计原则-->4.1、城堡游戏-->文档26.城堡游戏
  * 网易云课堂-->Java应用基础：设计篇-->第4章、设计原则-->4.2、消除代码复制-->视频30.消除代码复制
+ * 网易云课堂-->Java应用基础：设计篇-->第4章、设计原则-->4.3、封装-->视频33.封装
  */
-
 
 
 
@@ -60,20 +60,8 @@ public class Game {
 
     private void goRoom(String direction) 
     {
-        Room nextRoom = null;
-        if(direction.equals("north")) {
-            nextRoom = currentRoom.northExit;
-        }
-        if(direction.equals("east")) {
-            nextRoom = currentRoom.eastExit;
-        }
-        if(direction.equals("south")) {
-            nextRoom = currentRoom.southExit;
-        }
-        if(direction.equals("west")) {
-            nextRoom = currentRoom.westExit;
-        }
-
+        Room nextRoom = currentRoom.getExit(direction);
+        
         if (nextRoom == null) {
             System.out.println("那里没有门！");
         }
@@ -86,14 +74,7 @@ public class Game {
     public void showPrompt() {
     	System.out.println("你在" + currentRoom);
         System.out.print("出口有: ");
-        if(currentRoom.northExit != null)
-            System.out.print("north ");
-        if(currentRoom.eastExit != null)
-            System.out.print("east ");
-        if(currentRoom.southExit != null)
-            System.out.print("south ");
-        if(currentRoom.westExit != null)
-            System.out.print("west ");
+        System.out.print(currentRoom.getExitDesc());
         System.out.println();
     }
 	
